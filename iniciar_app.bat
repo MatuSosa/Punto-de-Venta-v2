f49afaa967bf172309131b884e679f80e80c5540
Francisco Paredes <paredesfrancisco031@gmail.com>
2025-01-22 21:28:38 -0300

    Fix procesos en segundo plano + 1 bug

--- iniciar_app.bat
+++ iniciar_app.bat
@@ -1,15 +1,16 @@
+REM //BUG: no inicia sesion en la bd
 @echo off
-
-REM Obtener la ruta del script actual y usarla como base
 set "InstallDir=%~dp0"
 
-REM Iniciar el servidor PHP sin mostrar la consola
+REM Iniciar el servidor PHP en segundo plano
 start /b "Servidor PHP" "%InstallDir%\php\php.exe" -S localhost:8000 -t "%InstallDir%"
 
 :: Esperar unos segundos para asegurarse de que el servidor PHP se haya iniciado
 timeout /t 5 /nobreak >nul
 
-:: Iniciar la aplicación Electron sin mostrar la consola
-start /b "" cmd /c "npm start"
+:: Iniciar Electron y esperar a que se cierre
+cmd /c "npm start"
 
+:: Una vez que Electron se cierra, cerrar PHP
+taskkill /f /im php.exe
 exit
